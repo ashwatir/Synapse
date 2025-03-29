656f3538caf25ab2ccc66909f83119eb3bf31c5e
Ashwati Rao <61846618+ashwatir@users.noreply.github.com>
2025-03-30 00:56:57 +0530

    Delete sqlscript/create schema.sql

--- sqlscript/create schema.sql
+++
@@ -1,6 +0,0 @@
--- SELECT * FROM OPENROWSET(
---     BULK 'https://awprojectasr.dfs.core.windows.net/silver/AdventureWorks_Calendar/',
---     FORMAT='PARQUET'
--- ) as query1
-
-CREATE SCHEMA gold;
